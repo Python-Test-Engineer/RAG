9d15af5e2cede5b6d5fcd718ca99b6dafe70479a
Craig West <iwswordpress@gmail.com>
2024-11-18 18:37:00 +0000

    sync bed to desk

--- table.sql
+++ table.sql
@@ -1,3 +1,3 @@
 CREATE EXTENSION IF NOT EXISTS vector;
 
-CREATE TABLE items (id bigserial PRIMARY KEY, track varchar(255), embedding vector(1536));
+CREATE TABLE embeddings_table (id bigserial PRIMARY KEY, track varchar(255), embeddings vector(1536));
