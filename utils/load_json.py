9690327e62aafda1b553175b4c7e301d6a5214cb
Craig West <iwswordpress@gmail.com>
2024-12-05 17:30:36 +0000

    load json

---
+++ utils/load_json.py
@@ -0,0 +1,37 @@
+import json
+from pprint import pprint
+
+
+# with open("./test.json") as file:
+#     json_data = json.load(file)
+
+
+# print(len(json_data))
+
+
+# for i in range(len(json_data)):
+#     if json_data[i]["type"] == "Image":
+#         print(i)
+#         pprint(json_data[i])
+#         break
+
+
+# for i in range(len(json_data)):
+#     pprint(json_data[i])
+#     print("\n")
+#     print("\n")
+
+
+def load_json(filename):
+    with open(filename) as f:
+        json_data = json.load(f)
+        return json_data
+
+
+json_data = load_json("./data/test.json")
+print(f"Number of items: {len(json_data)}")
+for i in range(len(json_data)):
+    if json_data[i]["type"] == "Image":
+        print(i)
+        pprint(json_data[i])
+        break
--- utils/test_base84.py
+++ utils/test_base84.py
@@ -1,12 +1,19 @@
 import base64
 
-with open("test.png", "rb") as image_file:
-    encoded_image_data = base64.b64encode(image_file.read())
-    print(encoded_image_data)
 
-# print(f'<img src="data:image/png;base64, {encoded_image_data.decode("utf-8")}" />')
+def encode_image(image):
+    with open(image, "rb") as image_file:
+        encoded_image_data = base64.b64encode(image_file.read())
+    return encoded_image_data
 
-decoded_image_data = base64.b64decode(encoded_image_data)
 
-with open("test2.png", "wb") as new_image_file:
-    new_image_file.write(decoded_image_data)
+def decode_image(encoded_image_data, image):
+    decoded_image_data = base64.b64decode(encoded_image_data)
+    with open(image, "wb") as new_image_file:
+        new_image_file.write(decoded_image_data)
+    return decoded_image_data
+
+
+if __name__ == "__main__":
+    encoded_image_data = encode_image("test.png")
+    decode_image(encoded_image_data, "test_output.png")
