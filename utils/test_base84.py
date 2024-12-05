a0bd3634319d43da1c6b153f3fc8fc2d72d715d8
Craig West <iwswordpress@gmail.com>
2024-12-05 16:14:12 +0000

    test base64

--- utils/test_base84.py
+++ utils/test_base84.py
@@ -4,7 +4,7 @@ with open("test.png", "rb") as image_file:
     encoded_image_data = base64.b64encode(image_file.read())
     print(encoded_image_data)
 
-print(f'<img src="data:image/png;base64, {encoded_image_data.decode("utf-8")}" />')
+# print(f'<img src="data:image/png;base64, {encoded_image_data.decode("utf-8")}" />')
 
 decoded_image_data = base64.b64decode(encoded_image_data)
 
