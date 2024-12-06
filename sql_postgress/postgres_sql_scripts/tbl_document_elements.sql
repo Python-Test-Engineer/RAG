699b42283883c2e22390beb4ca6fa9c7e059f033
Craig West <iwswordpress@gmail.com>
2024-12-06 19:01:48 +0000

    tables sql

--- sql_postgress/postgres_sql_scripts/tbl_document_elements.sql
+++ sql_postgress/postgres_sql_scripts/tbl_document_elements.sql
@@ -4,16 +4,18 @@ CREATE TABLE IF NOT EXISTS tbl_doc_elements (
     element_id VARCHAR(150),
     element_type VARCHAR(50),
     element_text TEXT,
+    parent_id VARCHAR(150),
     page_number INTEGER,
     filename VARCHAR(150),
     filetype VARCHAR(50),
     processed_text TEXT,
     keywords TEXT [],
+    ner TEXT [], 
     embedding REAL [],
     image_base64 TEXT,
     CONSTRAINT doc_elements_pk PRIMARY KEY (id)
 );
 
 INSERT INTO tbl_doc_elements (
-	file_id, element_id, element_type, element_text, image_base64, embedding)
-VALUES ('5f524c61-ef85-404e-b487-fb0fe304d8e2','be34cd2d71310ec72bfef3d1be2b2b36', 'TEXT', 'Hello World','iVBORw0KG', ARRAY[1, 2]);
+	file_id, element_id, element_type, element_text, parent_id, page_number,image_base64, embedding)
+VALUES ('5f524c61-ef85-404e-b487-fb0fe304d8e2','be34cd2d71310ec72bfef3d1be2b2b36', 'TEXT', 'Hello World','be34cd2d71310e',2,'iVBORw0KG', ARRAY[1, 2]);
