19e02cf62beeae0c77b17ef9bae5e8da8871ce36
Craig West <iwswordpress@gmail.com>
2024-12-07 14:49:57 +0000

    renamed file

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
-# app.py uses modules from utils folder
-# other folders are org functions
+
+from utils.test_base64 import encode_image, decode_image
+
+
+if __name__ == "__main__":
+    encoded_image_data = encode_image("test.png")
+    print(encoded_image_data)
+    decode_image(encoded_image_data, "test_output.png")
