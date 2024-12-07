31bc1dfe976d2fb7aae948fba687340eaddbc648
Craig West <iwswordpress@gmail.com>
2024-12-07 14:59:27 +0000

    edit base64 file

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-
+import random
 from utils.test_base64 import encode_image, decode_image
 
-
 if __name__ == "__main__":
-    encoded_image_data = encode_image("test.png")
+    encoded_image_data = encode_image("./data/test.png")
     print(encoded_image_data)
-    decode_image(encoded_image_data, "test_output.png")
+    decode_image(encoded_image_data, f"test_output_{random.randint(100, 900)}.png")
