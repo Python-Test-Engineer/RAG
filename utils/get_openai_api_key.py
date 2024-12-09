071cc6093f504ac542271cf4cb831c89fb522dc0
Craig West <iwswordpress@gmail.com>
2024-12-09 09:26:50 +0000

    load

--- utils/get_openai_api_key.py
+++ utils/get_openai_api_key.py
@@ -9,31 +9,18 @@ from rich.console import Console
 console = Console()
 load_dotenv(find_dotenv())
 
-if load_dotenv():
-    console.print(f"[green]Success: .env file found with some environment variables[/]")
-else:
-    console.print(
-        "[red]Caution: No environment variables found. Please create .env file in the root directory or add environment variables in the .env file[/red]"
-    )
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-client = OpenAI()
+def get_test_key():
 
-if OPENAI_API_KEY:
-    try:
-        client.models.list()
-        console.print(f"[cyan]OPENAI_API_KEY {OPENAI_API_KEY} is set and is valid[/]")
-    except openai.APIError as e:
-        print(f"[red]OpenAI API returned an API Error: {e}[/]")
-        pass
-    except openai.APIConnectionError as e:
-        print(f"[red]Failed to connect to OpenAI API: {e}[/]")
-        pass
-    except openai.RateLimitError as e:
-        print(f"[red]OpenAI API request exceeded rate limit: {e}[/]")
-        pass
+    if load_dotenv():
+        console.print(
+            f"[green]Success: .env file found with some environment variables[/]"
+        )
+    else:
+        console.print(
+            "[red]Caution: No environment variables found. Please create .env file in the root directory or add environment variables in the .env file[/red]"
+        )
 
-else:
-    print(
-        "[red]Please set you OpenAI API key as an environment variable OPENAI_API_KEY[/]"
-    )
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+    return OPENAI_API_KEY
