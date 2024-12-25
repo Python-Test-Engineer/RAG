e4896aa1d82da4f3e4ace9d05845072f0f1ddea8
Craig West <iwswordpress@gmail.com>
2024-12-25 07:28:30 +0000

    pgvector

---
+++ insert.sql
@@ -0,0 +1,4 @@
+
+
+
+INSERT INTO public.movie (adult,belongs_to_collection,budget,genres,homepage,id,imdb_id,original_language,original_title,overview,overview_vector,popularity,poster_path,production_companies,production_countries,release_date,revenue,runtime,spoken_languages,status,tagline,title,video,vote_average,vote_count) VALUES
