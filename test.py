e28cc2cf34af65d7b2fbb749604a39931c15d776
Craig West <iwswordpress@gmail.com>
2024-12-25 00:13:36 +0000

    test

---
+++ test.py
@@ -0,0 +1,13 @@
+import os
+import json
+
+from openai import OpenAI
+from dotenv import find_dotenv, load_dotenv
+from uuid import uuid4
+from random import randint
+from utils.get_openai_api_key import get_test_key
+from utils.get_postgres_connection import _conn_open
+from utils.load_json import load_json
+from rich.console import Console
+
+print("DONE")
