373a9fceca1ed55b5fee1dd7b4a689cc13a41538
Craig West <iwswordpress@gmail.com>
2024-12-28 00:55:00 +0000

    unstructured proces FULL OK

--- utils/get_save_base64.py
+++ utils/get_save_base64.py
@@ -18,5 +18,5 @@ def decode_image(encoded_image_data, image):
 
 if __name__ == "__main__":
     encoded_image_data = encode_image("./test01.png")
-    print(encoded_image_data)
+    # print(encoded_image_data)
     decode_image(encoded_image_data, "./test_output_utils.png")
